c5af05bc1c315c592f3ce548d20ddc80ca15eb6e
vonroga <ryanroga@gmail.com>
2022-07-19 13:00:02 -0700

    Added Copy

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,19 @@
 </script>
 
 <template>
-  <h1>Brand and Marketing Resources</h1>
-  <h2>Logos</h2>
+  <h1>Career Finder &amp; Market Outlook Tool</h1>
+  <h2>Plan your future, prepare for success.</h2>
   <p>
-    Each download package contains a PDF and a JPG of the logo along with a PDF
-    that outlines how to appropriately use the VIU logo. If you require a
-    different file format or a less common variant of VIU's logo, please contact
-    the Strategic Marketing department.
+    This tool can help you connect VIU programs and credentials with real world
+    career opportunities. Click a career to further explore a deeper description
+    of the industry, educational requirements, and the perhaps most importantly,
+    the market outlook. Outlook data is provided by the Government of Canada
+    Labour Market Information API and reflects a 3-year employment outlook by
+    occupation.
   </p>
-  <a href="#">Test link</a>
+  <div class="app-area">
+    <router-view></router-view>
+  </div>
 </template>
 
 <style>
@@ -28,6 +32,18 @@
   min-height: 100vh;
 }
 
+h1 {
+  margin: 0 0 1rem !important;
+}
+
+h2 {
+  margin: 0 0 0.5rem !important;
+}
+
+p {
+  margin: 0 0 10px !important;
+}
+
 a,
 a:visited,
 a:active {
@@ -41,11 +57,11 @@ a:hover {
 }
 
 h1 {
-  @apply text-3xl md:text-4xl lg:text-5xl !important;
+  @apply text-3xl md:text-4xl lg:text-5xl font-bold !important;
 }
 
 h2 {
-  @apply text-2xl md:text-3xl lg:text-4xl !important;
+  @apply text-2xl md:text-3xl lg:text-4xl font-semibold !important;
 }
 
 h1,
@@ -55,5 +71,10 @@ h4,
 h5,
 h6 {
   color: #003b5c;
+}
+
+.app-area {
+  margin: 1rem 0 0 0;
+  width: full;
 }
 </style>
