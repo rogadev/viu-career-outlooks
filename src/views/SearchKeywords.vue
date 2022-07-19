3c952bfa8fab7096f8a631f0c013c52cc1bb4e9a
vonroga <ryanroga@gmail.com>
2022-07-19 13:00:49 -0700

    Create SearchKeywords.vue

---
+++ src/views/SearchKeywords.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const nid = route.params.nid; // expects a VIU nid on params - see router
+</script>
+
+<template>
+  <div>
+    <h1>Search by Program</h1>
+    <p>...</p>
+  </div>
+</template>
