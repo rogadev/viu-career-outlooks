acaa55f04323596f5b66eb53c2393fa9e1ec6ce8
vonroga <ryanroga@gmail.com>
2022-07-19 13:00:28 -0700

    Create Router

---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+// USES VUE-ROUTER@4
+import { createWebHistory, createRouter } from 'vue-router'
+
+// ROUTE COMPONENTS (Lazy Loading)
+const Home = () => import('../views/Home.vue')
+const SearchProgram = () => import('../views/SearchProgram.vue')
+const SearchKeywords = () => import('../views/SearchKeywords.vue')
+const PrefetchSearchProgram = () => import('../views/PrefetchSearchProgram.vue')
+const PrefetchSearchKeywords = () =>
+  import('../views/PrefetchSearchKeywords.vue')
+const NotFound = () => import('../views/NotFound.vue')
+
+// ROUTES
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/search-by-program',
+    name: 'SearchProgram',
+    component: SearchProgram,
+  },
+  {
+    path: '/search-by-keywords',
+    name: 'SearchKeywords',
+    component: SearchKeywords,
+  },
+  {
+    path: '/search-by-program/:nid',
+    name: 'PrefetchSearchProgram',
+    component: PrefetchSearchProgram,
+  },
+  {
+    path: '/search-by-keywords/:keywords',
+    name: 'PrefetchSearchKeywords',
+    component: PrefetchSearchKeywords,
+  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+]
+
+// ROUTER
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
