6fddc50134212715863a9cdc4e7a334a7d9b69f4
vonroga <ryanroga@gmail.com>
2022-07-19 22:39:46 -0700

    Update SearchProgram.vue

--- src/views/SearchProgram.vue
+++ src/views/SearchProgram.vue
@@ -3,12 +3,12 @@
 import { ref } from "vue";
 import { useRoute } from "vue-router";
 const route = useRoute();
-const nid = route.params.nid; // expects a VIU nid on params - see router
+const nid = route.params.nid || ""; // expects a VIU nid on params - see router
 </script>
 
 <template>
   <div>
     <h1>Search by Program</h1>
-    <p>...</p>
+    <p>{{ nid }}</p>
   </div>
 </template>
