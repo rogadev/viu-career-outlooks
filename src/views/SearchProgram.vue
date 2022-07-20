098503a64611d2c63d23e11ff1f46b76ee83245c
Roga <ryanroga@gmail.com>
2022-07-20 14:39:56 -0700

    Update SearchProgram.vue

--- src/views/SearchProgram.vue
+++ src/views/SearchProgram.vue
@@ -2,13 +2,32 @@
 <script setup>
 import { ref } from "vue";
 import { useRoute } from "vue-router";
+// import data from "../assets/temp_programs.json";
+
 const route = useRoute();
 const nid = route.params.nid || ""; // expects a VIU nid on params - see router
+
+const programs =
+  import.meta.env.MODE === "PROD"
+    ? await fetch("https://api.viu.ca/v1/programs").json()
+    : (await import("../assets/temp_programs.json")).default;
+
+// programs.value = data;
+console.log(programs);
 </script>
 
 <template>
   <div>
     <h1>Search by Program</h1>
+    <select name="program" id="program">
+      <option
+        v-for="program of programs"
+        :value="program.nid"
+        :id="program.nid"
+      >
+        {{ program.title }}
+      </option>
+    </select>
     <p>{{ nid }}</p>
   </div>
 </template>
