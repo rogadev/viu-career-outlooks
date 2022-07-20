43a932f164c7fd69d0f4910b5eb45a6db6e1ddb0
Roga <ryanroga@gmail.com>
2022-07-20 15:13:16 -0700

    Update SearchProgram.vue

--- src/views/SearchProgram.vue
+++ src/views/SearchProgram.vue
@@ -5,21 +5,25 @@ import { useRoute } from "vue-router";
 // import data from "../assets/temp_programs.json";
 
 const route = useRoute();
-const nid = route.params.nid || ""; // expects a VIU nid on params - see router
+const nid = ref(null);
+nid.value = route.params.nid || ""; // expects a VIU nid on params - see router
 
 const programs =
   import.meta.env.MODE === "PROD"
     ? await fetch("https://api.viu.ca/v1/programs").json()
     : (await import("../assets/temp_programs.json")).default;
 
-// programs.value = data;
-console.log(programs);
+const jobs = ref([]);
+jobs.value = await fetch(
+  `https://viu-career-outlook.herokuapp.com/api/v1/jobs/program/${nid.value}`
+).json();
+console.log(jobs.value);
 </script>
 
 <template>
   <div>
     <h1>Search by Program</h1>
-    <select name="program" id="program">
+    <select name="program" id="program" v-bind="nid">
       <option
         v-for="program of programs"
         :value="program.nid"
