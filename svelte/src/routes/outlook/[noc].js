3308e83f1c580e8b0542be158b72645a12fad3d3
vonroga <ryanroga@gmail.com>
2022-07-28 14:32:35 -0700

    Update [noc].js
    testing

--- svelte/src/routes/outlook/[noc].js
+++ svelte/src/routes/outlook/[noc].js
@@ -1,9 +1,14 @@
-export async function GET() {
-  // Fetch from our heroku api.
+/**
+ * Shadow Endpoing - handles getting job outlook data for a given NOC code. Querries our express api, proxys to the LMI-EO api, and returns the data.
+ * @param {*} param0
+ */
+export async function GET({ params }) {
+  const noc = Number(params.noc)
+
   return {
     status: 200,
     body: {
-      title: 'Careers',
+      title: noc,
     },
   }
 }
