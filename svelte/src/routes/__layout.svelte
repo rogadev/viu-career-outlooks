9a3c82d0ccaf49e572ab9c3d13ef0e96ea7ee0ac
vonroga <ryanroga@gmail.com>
2022-07-28 14:01:54 -0700

    Create __layout.svelte

---
+++ svelte/src/routes/__layout.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Navbar from '../components/Navbar.svelte'
+</script>
+
+<svelte:head>
+  <title>App Title</title>
+</svelte:head>
+
+<div class="app-wrapper">
+  <Navbar />
+  <div class="slot-wrapper">
+    <slot />
+  </div>
+  <footer>
+    <p>footer will go here</p>
+  </footer>
+</div>
+
+<style>
+  div.app-wrapper {
+    font-family: 'ProximaNova', Corbel, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #222;
+    font-size: 18px;
+    border: 1rem solid #003b5c;
+    margin: 0;
+    background-color: #fff;
+    min-height: 100vh;
+  }
+
+  div.slot-wrapper {
+    padding: 1.5rem;
+  }
+
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 2.5rem;
+  }
+</style>
