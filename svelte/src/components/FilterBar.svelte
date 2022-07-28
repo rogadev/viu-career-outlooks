fd37da0023fda4b9ec1a7ed180960c4151d32f58
vonroga <ryanroga@gmail.com>
2022-07-27 20:39:30 -0700

    Create FilterBar.svelte

---
+++ svelte/src/components/FilterBar.svelte
@@ -0,0 +1,9 @@
+<p>
+  This will be another input field that further reduces the number of search
+  results.
+</p>
+
+<p>
+  Use debouncing, respond to the parent with the new filtered search term.
+  Parent will handle filtering down results.
+</p>
