683db64472760077752da1121b9db30c072d2578
vonroga <ryanroga@gmail.com>
2022-07-28 14:02:57 -0700

    Update index.svelte

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,11 +1,19 @@
-<h1>VIU Career Outlooks Tool</h1>
+<div>
+  <div class="w-4/5 mx-auto">
+    <h1>Career Finder &amp; Market Outlook Tool</h1>
+    <h2>Plan your future, prepare for success.</h2>
+    <p>
+      This tool can help you connect VIU programs and credentials with real
+      world career opportunities. Click a career to further explore a deeper
+      description of the industry, educational requirements, and the perhaps
+      most importantly, the market outlook. Outlook data is provided by the
+      Government of Canada Labour Market Information API and reflects a 3-year
+      employment outlook by occupation.
+    </p>
+  </div>
+</div>
 
-<p>
-  First, list the available types of search. Search by program or search by
-  credential.
-</p>
-
-<h2>Routes follows:</h2>
+<h2>Routes:</h2>
 
 <ul>
   <li>/outlook/:noc</li>
