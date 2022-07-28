9d9b394dbca1a9139d592d2bc0eda55615a9fde5
vonroga <ryanroga@gmail.com>
2022-07-27 20:39:43 -0700

    Update index.svelte

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,9 +1,18 @@
-<script>
-  import JobModal from '../components/JobModal.svelte'
-</script>
+<h1>VIU Career Outlooks Tool</h1>
 
-<h1>Welcome to SvelteKit</h1>
 <p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  First, list the available types of search. Search by program or search by
+  credential.
 </p>
-<JobModal />
+
+<h2>Routes follows:</h2>
+
+<ul>
+  <li>/outlook/:noc</li>
+</ul>
+
+<ul>
+  <li>/careers/search</li>
+  <li>/careers/by-program</li>
+  <li>/careers/by-program/:nid</li>
+</ul>
