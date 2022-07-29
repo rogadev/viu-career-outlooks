194a27107812cb345b041a4ef803a9de14e8e723
vonroga <ryanroga@gmail.com>
2022-07-29 09:35:45 -0700

    Update [noc].svelte
    Just trying to get data on the screen...

--- svelte/src/routes/outlook/[noc].svelte
+++ svelte/src/routes/outlook/[noc].svelte
@@ -1,15 +1,26 @@
 <script>
-  export /** @type {object} */ let noc
-  export /** @type {any} */ let province
-  export /** @type {any} */ let eruid
-  export /** @type {any} */ let trends
-  export /** @type {any} */ let outlook_verbose
+  export let noc
+
+  $: console.log(noc)
 </script>
 
+<!-- 
+
+  {
+        noc,
+        title: results[1].title,
+        jobs: results[1].jobs,
+        requirements: results[1].requirements,
+        duties: results[1].duties,
+        outlook: results[0].outlook,
+        outlook_verbose: results[0].outlook_verbose,
+        trends: results[0].trends,
+        province: 'British Columbia',
+        eruid: 5900,
+      }
+
+ -->
+
 <ul>
-  <li>{noc}</li>
-  <li>{province}</li>
-  <li>{eruid}</li>
-  <li>{trends}</li>
-  <li>{outlook_verbose}</li>
+  <li>lkj</li>
 </ul>
