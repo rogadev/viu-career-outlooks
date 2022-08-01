ce5f3ac3964a2506a011b676f494411b1b96204b
vonroga <ryanroga@gmail.com>
2022-08-01 13:07:42 -0700

    Added First Tests

---
+++ svelte/src/lib/helpers/titleCase.test.ts
@@ -0,0 +1,22 @@
+import { it, expect } from 'vitest'
+import { titleCase } from './titleCase.js'
+
+it('Correctly title cases a given string', () => {
+  expect(titleCase('hello world')).toBe('Hello World')
+  expect(titleCase('hello World')).toBe('Hello World')
+  expect(titleCase('hello. world.')).toBe('Hello. World.')
+  expect(titleCase('hello-world')).toBe('Hello-World')
+  expect(titleCase('hello_world')).toBe('Hello_World')
+})
+
+it('Correctly ignores anything inside of brackets', () => {
+  expect(titleCase('tim cook (ceo)')).toBe('Tim Cook (ceo)')
+  expect(titleCase('tim-cook (ceo)')).toBe('Tim-Cook (ceo)')
+  expect(titleCase('tim_cook (ceo)')).toBe('Tim_Cook (ceo)')
+  expect(titleCase("I'm a little teapot (and you're not)")).toBe(
+    "I'm A Little Teapot (and you're not)"
+  )
+  expect(titleCase('abc 123. this-is-a-test. (CEO)')).toBe(
+    'Abc 123. This-Is-A-Test. (CEO)'
+  )
+})
