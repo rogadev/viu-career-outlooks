91697a0836d8aff82010f25ed35ba2d671df9080
Roga <ryanroga@gmail.com>
2022-08-02 11:58:28 -0700

    Update Navbar.svlete - Making Responsive

--- svelte/src/lib/components/Navbar.svelte
+++ svelte/src/lib/components/Navbar.svelte
@@ -1,4 +1,4 @@
-<header>
+<header class="block md:flex md:justify-between md:items-center">
   <div class="flex justify-between">
     <a id="brand-link" href="/">
       <div class="flex gap-3 items-center">
@@ -8,17 +8,25 @@
           alt="VIU Logo"
           class="h-[70px]"
         />
-        <span id="brand-title" class=" font-extrabold text-3xl"
-          >Career Outlook Tool</span
-        >
+        <h1 id="brand-title" class=" font-extrabold text-3xl">
+          Career Outlook Tool
+        </h1>
       </div>
     </a>
   </div>
-  <nav aria-label="Main">
-    <ul>
-      <li><a href="/outlook/2175">X</a></li>
-      <li><a href="/careers/search">Search Credentials</a></li>
-      <li><a href="/careers/by-program">Search by Program</a></li>
+  <nav aria-label="Main" class="text-xl md:text-2xl">
+    <ul class="flex gap-4 items-center">
+      <li class="nav-item"><a href="/outlook/2175">X</a></li>
+      <li class="nav-item"><a href="/careers/search">Search Credentials</a></li>
+      <li class="nav-item">
+        <a href="/careers/by-program">Search by Program</a>
+      </li>
     </ul>
   </nav>
 </header>
+
+<style lang="postcss">
+  .nav-item {
+    @apply px-4 py-2 text-gray-600 hover:text-gray-900 hover:bg-gray-100;
+  }
+</style>
