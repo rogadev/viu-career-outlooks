d47044b717341ba3951794e8177ae2c023d809ff
Roga <ryanroga@gmail.com>
2022-08-02 11:21:00 -0700

    Major Navbar Styling Changes

--- svelte/src/lib/components/Navbar.svelte
+++ svelte/src/lib/components/Navbar.svelte
@@ -1,66 +1,24 @@
-<nav aria-label="Main">
-  <a id="brand-link" href="/">
-    <div class="brand">
-      <img id="brand-logo" src="/img/viu_logo.svg" alt="VIU Logo" />
-      <span id="brand-title">VIU Career Finder & Market Outlook Tool</span>
-    </div>
-  </a>
-  <ul>
-    <li><a href="/outlook/2175">X</a></li>
-    <li><a href="/careers/search">Search Credentials</a></li>
-    <li><a href="/careers/by-program">Search by Program</a></li>
-  </ul>
-</nav>
-
-<style>
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 1rem;
-    text-align: center;
-  }
-  a {
-    color: #222;
-    text-decoration: none;
-    font-size: 1.25rem;
-    font-weight: bold;
-    padding: 0.5rem 1rem;
-    border-radius: 0.25rem;
-  }
-  li > a {
-    background-color: #f5f5f5;
-    border: 1px solid #ccc;
-    box-shadow: 0 0 0.25rem 0 rgba(131, 131, 131, 0.125);
-  }
-  a:hover {
-    color: white;
-    background-color: #003b5c;
-  }
-  nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid #06476d;
-    background-color: rgb(249, 249, 249);
-  }
-  .brand {
-    display: flex;
-    align-items: center;
-  }
-  #brand-title {
-    font-size: 1.5rem;
-    font-weight: bold;
-    padding-left: 1rem;
-  }
-  #brand-logo {
-    width: 50px;
-    height: 50px;
-  }
-  #brand-link:hover {
-    background-color: inherit;
-    color: #222;
-  }
-</style>
+<header>
+  <div class="flex justify-between">
+    <a id="brand-link" href="/">
+      <div class="flex gap-3 items-center">
+        <img
+          id="brand-logo"
+          src="/img/viu_logo.svg"
+          alt="VIU Logo"
+          class="h-[70px]"
+        />
+        <span id="brand-title" class=" font-extrabold text-3xl"
+          >Career Outlook Tool</span
+        >
+      </div>
+    </a>
+  </div>
+  <nav aria-label="Main">
+    <ul>
+      <li><a href="/outlook/2175">X</a></li>
+      <li><a href="/careers/search">Search Credentials</a></li>
+      <li><a href="/careers/by-program">Search by Program</a></li>
+    </ul>
+  </nav>
+</header>
