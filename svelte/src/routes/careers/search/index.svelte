eff58b88360ab8ecfd0b0e785ed436eba9a5735d
vonroga <ryanroga@gmail.com>
2022-08-02 10:42:36 -0700

    Update index.svelte

--- svelte/src/routes/careers/search/index.svelte
+++ svelte/src/routes/careers/search/index.svelte
@@ -1,3 +1,9 @@
+<script>
+  import SearchBar from '$lib/components/SearchBar.svelte'
+</script>
+
+<SearchBar />
+
 <p>This will be the search page.</p>
 
 <p>Load an empty screen with nothing but a search box.</p>
