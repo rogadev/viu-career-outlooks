7b44f4d017bf4e795a225bc2c90b95524e433367
vonroga <ryanroga@gmail.com>
2022-08-03 10:30:06 -0700

    Styling Update Navbar

--- svelte/src/lib/components/Navbar.svelte
+++ svelte/src/lib/components/Navbar.svelte
@@ -1,32 +1,39 @@
 <header class="block md:flex md:justify-between md:items-center">
-  <div class="flex justify-between">
-    <a id="brand-link" href="/">
-      <div class="flex gap-3 items-center">
-        <img
-          id="brand-logo"
-          src="/img/viu_logo.svg"
-          alt="VIU Logo"
-          class="h-[70px]"
-        />
-        <h1 id="brand-title" class=" font-extrabold text-3xl">
-          Career Outlook Tool
-        </h1>
-      </div>
-    </a>
-  </div>
+  <a
+    id="brand-link"
+    href="/"
+    class="flex items-center justify-between md:justify-start"
+  >
+    <img
+      id="brand-logo"
+      src="/img/viu_logo.svg"
+      alt="VIU Logo"
+      class="h-[70px]"
+    />
+    <h1
+      id="brand-title"
+      class="font-extrabold text-3xl text-center mx-auto md:text-left md:ml-4"
+    >
+      Career Outlook Tool
+    </h1>
+  </a>
+
   <nav aria-label="Main" class="text-xl md:text-2xl">
-    <ul class="flex gap-4 items-center">
-      <li class="nav-item"><a href="/outlook/2175">X</a></li>
+    <ul
+      class="flex gap-4 items-center mr-4 justify-evenly md:justify-start mt-4 md:mt-0 text-center"
+    >
+      <!-- <li class="nav-item"><a href="/outlook/2175">X</a></li> -->
       <li class="nav-item"><a href="/careers/search">Search Credentials</a></li>
       <li class="nav-item">
-        <a href="/careers/by-program">Search by Program</a>
+        <a href="/careers/by-program">Search Program</a>
       </li>
     </ul>
   </nav>
 </header>
 
 <style lang="postcss">
+  /* has an error in my IDE for hover: but it works in testing. */
   .nav-item {
-    @apply px-4 py-2 text-gray-600 hover:text-gray-900 hover:bg-gray-100;
+    @apply px-4 py-2 text-gray-600 hover:text-gray-900 hover:bg-gray-100 text-2xl md:text-xl font-semibold;
   }
 </style>
