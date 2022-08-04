5dd106e7d3ae54b8b865a69c0ab482359c822c90
vonroga <ryanroga@gmail.com>
2022-08-04 10:53:22 -0700

    Update Careers Search View
    Including Results component if state is "found", meaning the search request returned results.

--- src/routes/careers/search/index.svelte
+++ src/routes/careers/search/index.svelte
@@ -1,8 +1,13 @@
 <script>
   import SearchBar from '$lib/components/SearchBar.svelte'
+  import Results from '$lib/components/Results.svelte'
+  import { state } from '$lib/stores/searching.js'
 </script>
 
 <SearchBar />
+{#if $state === 'found'}
+  <Results />
+{/if}
 
 <p>This will be the search page.</p>
 
