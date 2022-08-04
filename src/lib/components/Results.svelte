16d165c337dab30037164587119cbe4e64b15864
vonroga <ryanroga@gmail.com>
2022-08-04 12:24:38 -0700

    Update Results.svelte

--- src/lib/components/Results.svelte
+++ src/lib/components/Results.svelte
@@ -1,5 +1,33 @@
 <script>
   import { results } from '$lib/stores/searching.js'
+
+  let jobObjects = $results.jobs
+  /**
+   * @type {Array<Object>}
+   */
+  let jobs = [{ noc: '', jobTitle: '' }]
+  jobObjects.forEach(
+    (/** @type {{ noc: string; jobs: Array<string>; }} */ obj) => {
+      let noc = obj.noc
+      let jobList = obj.jobs
+      jobList.forEach((/** @type {string} */ jobTitle) => {
+        jobs.push({
+          noc: noc,
+          jobTitle: jobTitle,
+        })
+      })
+    }
+  )
+  console.log(jobs)
 </script>
 
-<p>{results}</p>
+{#each jobs as job}
+  <div class="job">
+    <div class="noc">
+      {job.noc}
+    </div>
+    <div class="job-title">
+      {job.jobTitle}
+    </div>
+  </div>
+{/each}
