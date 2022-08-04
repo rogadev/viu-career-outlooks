21bf51b65266edaf02c4172ed5d4b648ef9a928b
vonroga <ryanroga@gmail.com>
2022-08-03 18:25:20 -0700

    Moved Search To Lib
    Search isn't really a helper but rather a lib function. Moved for clarity.

--- svelte/src/lib/search.js
+++ svelte/src/lib/search.js
@@ -1,10 +1,13 @@
 import { state, keywordFields } from '$lib/stores/searching.js'
 
+let credentialField = ''
+let keywordsField = ''
+
 // Reactively updates variables for use in our async get request
 let /** @type {string} */ credential, /** @type {string} */ keywords
-keywordFields.subscribe(({ credential: cred, keywords: kw }) => {
-  credential = cred
-  keywords = kw
+keywordFields.subscribe(({ credential, keywords }) => {
+  credentialField = credential.toLowerCase()
+  keywordsField = keywords.toLowerCase()
 })
 
 export const search = async () => {
@@ -14,16 +17,17 @@ export const search = async () => {
   let searchData = {}
   try {
     const url = new URL(
-      'https://viu-career-outlook.herokuapp.com/api/v1/jobs/credential'
+      'https://viu-career-outlook.herokuapp.com/api/v1/jobs-by-credential'
     )
-    url.searchParams.set('credentail', credential)
-    url.searchParams.set('keywords', keywords)
+    url.searchParams.set('credential', credentialField)
+    url.searchParams.set('keywords', keywordsField)
 
     const response = await fetch(url)
     searchData = await response.json()
   } catch (e) {
     // @ts-ignore
     const error = new Error(e)
+    console.error(e)
     state.set(error)
     return {
       status: 500,
