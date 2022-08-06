a24c5c014a87b4c07ff3444a1cd75309c8882547
Roga <ryanroga@gmail.com>
2022-08-06 13:20:12 -0700

    Update FilterBar.svelte
    Extracted the logic for filtering searches into the filtered results store.

--- src/lib/components/FilterBar.svelte
+++ src/lib/components/FilterBar.svelte
@@ -1,22 +1,9 @@
 <script>
-  //import { jobs as results } from '$lib/stores/searching.js'
+  import { results } from '$lib/stores/filter.js'
 
-  //  let allSearchResults = $results
+  let input = ''
 
-  let filterInput = ''
-
-  let filteredResults
-
-  // $: {
-  //   if (filterInput === '') filteredResults = allSearchResults
-  //   else {
-  //     console.log(allSearchResults)
-  //     filteredResults = allSearchResults.filter((result) => {
-  //       return result.title.toLowerCase().includes(filterInput.toLowerCase())
-  //     })
-  //     console.log(filteredResults)
-  //   }
-  // }
+  $: () => results.filterFor(input)
 </script>
 
-<input type="text" id="filter-input" bind:value={filterInput} />
+<input type="text" id="filter-input" bind:value={input} />
