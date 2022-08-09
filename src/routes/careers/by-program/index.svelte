4c418376b0f50e5fa3503bd7a7c764b2d0bf34c7
vonroga <ryanroga@gmail.com>
2022-08-09 11:39:24 -0700

    Dynamic Select List For Careers-By-Program Page
    Adds dynamic select list. Currently working on sending program search results to results store. Backend fix required.

--- src/routes/careers/by-program/index.svelte
+++ src/routes/careers/by-program/index.svelte
@@ -1,1 +1,37 @@
-<p>Search by program to go here</p>
+<script>
+  import { getPrograms, getProgram } from './programs'
+
+  /** @type {{nid:number, title:string}[]} */
+  let programs = []
+  /** @type {number} */
+  let selectedNid
+  /** @type {string} */
+  let keywords
+
+  async function fetchPrograms() {
+    programs = await getPrograms()
+  }
+
+  async function setProgram(/** @type {number} */ nid) {
+    if (!nid) return (keywords = '')
+    const data = await getProgram(nid)
+    console.log(data)
+  }
+
+  $: {
+    setProgram(selectedNid)
+  }
+</script>
+
+<h1>Search for career outlooks for any VIU program</h1>
+
+{#await fetchPrograms()}
+  <p>Loading...</p>
+{:then}
+  <select name="nid" id="nid" bind:value={selectedNid}>
+    <option value={null} selected>Select a program...</option>
+    {#each programs as program}
+      <option value={program.nid}>{program.title}</option>
+    {/each}
+  </select>
+{/await}
---
+++ src/routes/careers/by-program/programs.js
@@ -0,0 +1,29 @@
+// Extracted from .svelte file for testing.
+
+/**
+ * Fetch the searchable program list from our API.
+ * @returns
+ */
+const getPrograms = async () => {
+  try {
+    const response = await fetch(
+      'https://viu-career-outlook.herokuapp.com/api/v1/programs/searchable'
+    )
+    const data = await response.json()
+    return data
+  } catch (e) {
+    console.error(e)
+    // @ts-ignore
+    return new Error(e)
+  }
+}
+
+const getProgram = async (/** @type {number} */ nid) => {
+  const response = await fetch(
+    `https://viu-career-outlook.herokuapp.com/api/v1/jobs/program/${nid}`
+  )
+  const data = await response.json()
+  return data
+}
+
+export { getPrograms, getProgram }
