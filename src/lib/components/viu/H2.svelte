b128c7856c0a754b31e3b4be744efecc1ebf973a
vonroga <ryanroga@gmail.com>
2022-08-10 11:33:35 -0700

    Update Styling

--- src/lib/components/viu/H2.svelte
+++ src/lib/components/viu/H2.svelte
@@ -1,3 +1,17 @@
-<h2 class="font-extrabold text-viu-primary text-3xl leading-5 my-4">
+<h2>
   <slot />
 </h2>
+
+<style>
+  h2 {
+    margin-top: 1em;
+    color: #003b5c;
+    font-size: 30px;
+    font-weight: bold;
+    background-image: none;
+    font-style: normal;
+    line-height: 1.2;
+    letter-spacing: 0.02em;
+    margin: 0;
+  }
+</style>
