7c6d1ba5e79767f1520fbe62d187abee91e768e9
vonroga <ryanroga@gmail.com>
2022-08-16 10:56:02 -0700

    Implement New Programs Store

--- src/routes/programs.js
+++ src/routes/programs.js
@@ -1,29 +1,40 @@
-// Extracted from .svelte file for testing.
+import { programs } from '$lib/stores/programs'
+let programList = programs.subscribe((v) => v)
 
 /**
  * Fetch the searchable program list from our API.
  * @returns
  */
 const getPrograms = async () => {
+  if (!programList.length) {
+    try {
+      const response = await fetch(
+        'https://viu-career-outlook.herokuapp.com/api/v1/programs/searchable'
+      )
+      const { data } = await response.json()
+      return data
+    } catch (e) {
+      console.error(e)
+      // @ts-ignore
+      return new Error(e)
+    }
+  } else {
+    return programList
+  }
+}
+
+const getProgram = async (/** @type {number} */ nid) => {
   try {
     const response = await fetch(
-      'https://viu-career-outlook.herokuapp.com/api/v1/programs/searchable'
+      `https://viu-career-outlook.herokuapp.com/api/v1/jobs/program/${nid}`
     )
     const { data } = await response.json()
-    return data
+    return data ?? []
   } catch (e) {
     console.error(e)
     // @ts-ignore
     return new Error(e)
   }
-}
-
-const getProgram = async (/** @type {number} */ nid) => {
-  const response = await fetch(
-    `https://viu-career-outlook.herokuapp.com/api/v1/jobs/program/${nid}`
-  )
-  const { data } = await response.json()
-  return data
 }
 
 export { getPrograms, getProgram }
