ef2045ba4e8b054b881a583943bebf10a25ab1ff
Roga <ryanroga@gmail.com>
2022-08-31 15:08:02 -0700

    Adds Error Pages
    Adds custom styled error pages.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from '$app/stores'
+  import H1 from '$lib/components/viu/H1.svelte'
+  import H2 from '$lib/components/viu/H2.svelte'
+</script>
+
+<H1>Error {$page.status}</H1>
+<H2>{$page?.error?.message ?? 'Oops... Something went wrong!'}</H2>
