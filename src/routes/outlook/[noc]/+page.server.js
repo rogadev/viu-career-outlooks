6121230352b8e44c4b5348f59e17ce8f96e957d3
Roga <ryanroga@gmail.com>
2022-08-31 11:28:04 -0700

    Update +page.server.js

--- src/routes/outlook/[noc]/+page.server.js
+++ src/routes/outlook/[noc]/+page.server.js
@@ -13,6 +13,81 @@ import unitGroups from '$lib/server/data/noc_2016_unit_groups.json'
 const headers = new Headers()
 headers.append('USER_KEY', import.meta.env.VITE_GC_API_USER_KEY)
 
+/**
+ * @typedef Outlook
+ * @property {number} potential - The outlook potential, represented as a value from 0 to 3.
+ * @property {string} outlook_verbose - The verbose description of the outlook potential.
+ * @property {string} trends - The outlook trends, represented as a value from 0 to 3.
+ */
+
+/**
+ * The LMI-EO API uses a silly outlook potential rating schema where 0 is undetermined, 1 is good, 2 is limited, and 3 is fair. This function converts the outlook potential value to a more logical format. 3 is good, 2 is fair, 1 is limited, 0 is undetermined. Additionally adds a verbose description of the outlook potential.
+ * @param {Outlook} outlook - The outlook data to fix.
+ * @returns The fixed outlook data with corrected potential value.
+ */
+const refactorOutlookWithLogicalPotential = (outlook) => {
+  const potential = outlook.potential
+  let newPotential
+  switch (Number(potential)) {
+    case 1:
+      newPotential = 3
+      break
+    case 2:
+      newPotential = 1
+      break
+    case 3:
+      newPotential = 2
+      break
+    default:
+      newPotential = 0
+  }
+  return {
+    ...outlook,
+    potential: newPotential,
+    outlook_verbose: verbifyOutlookValue(newPotential),
+  }
+}
+
+/**
+ * Uses the LMI Employment Outlook API to query for the provincial outlook for a given NOC unit group code. LMI-EO uses NOC 2016 v1.3.
+ * @param {String} noc The NOC code of the relevant unit group.
+ * @param {String} provinceId The province code relevant to this search.
+ * @returns Object containing key value pairs of data related to the outlook of the given unit group in the given province.
+ */
+const fetchProvincialOutlook = async (noc, provinceId) => {
+  // example query: GET https://lmi-outlooks-esdc-edsc-apicast-production.api.canada.ca/clmix-wsx/gcapis/outlooks?noc=1111&rtp=1&rid=10
+
+  try {
+    const response = await fetch(
+      `https://lmi-outlooks-esdc-edsc-apicast-production.api.canada.ca/clmix-wsx/gcapis/outlooks?noc=${noc}&rtp=1&rid=${provinceId}&lang=en`,
+      { headers }
+    )
+    const data = await response.json()
+    return data
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+/**
+ * Verifies an outlook potential value form 0 to 3.
+ * @param {Number} potential - The outlook potential, represented as a value from 0 to 3.
+ * @returns The verbose description of the outlook potential.
+ */
+const verbifyOutlookValue = (potential) => {
+  switch (Number(potential)) {
+    case 1:
+      return 'Limited'
+    case 2:
+      return 'Fair'
+    case 3:
+      return 'Good'
+    default:
+      return 'Undetermined'
+  }
+}
+
+// LOAD FUNCTION
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
   loading.set(true)
@@ -120,41 +195,6 @@ export async function load({ params }) {
 // }
 
 /**
- * @typedef Outlook
- * @property {number} potential - The outlook potential, represented as a value from 0 to 3.
- * @property {string} outlook_verbose - The verbose description of the outlook potential.
- * @property {string} trends - The outlook trends, represented as a value from 0 to 3.
- */
-
-/**
- * The LMI-EO API uses a silly outlook potential rating schema where 0 is undetermined, 1 is good, 2 is limited, and 3 is fair. This function converts the outlook potential value to a more logical format. 3 is good, 2 is fair, 1 is limited, 0 is undetermined. Additionally adds a verbose description of the outlook potential.
- * @param {Outlook} outlook - The outlook data to fix.
- * @returns The fixed outlook data with corrected potential value.
- */
-function refactorOutlookWithLogicalPotential(outlook) {
-  const potential = outlook.potential
-  let newPotential
-  switch (Number(potential)) {
-    case 1:
-      newPotential = 3
-      break
-    case 2:
-      newPotential = 1
-      break
-    case 3:
-      newPotential = 2
-      break
-    default:
-      newPotential = 0
-  }
-  return {
-    ...outlook,
-    potential: newPotential,
-    outlook_verbose: verbifyOutlookValue(newPotential),
-  }
-}
-
-/**
  * Uses the LMI Employment Outlook API to query for the national outlook for a given NOC unit group code. LMI-EO uses NOC 2016 v1.3.
  * @param {String} noc The NOC code of the relevant unit group.
  * @returns Array of outlook objects containing provincial code and "potential" metric. Potential describes outlook as a number from 0-3, 3 being best.
@@ -166,42 +206,3 @@ function refactorOutlookWithLogicalPotential(outlook) {
 //     { headers: headers }
 //   )
 // }
-
-/**
- * Uses the LMI Employment Outlook API to query for the provincial outlook for a given NOC unit group code. LMI-EO uses NOC 2016 v1.3.
- * @param {String} noc The NOC code of the relevant unit group.
- * @param {String} provinceId The province code relevant to this search.
- * @returns Object containing key value pairs of data related to the outlook of the given unit group in the given province.
- */
-async function fetchProvincialOutlook(noc, provinceId) {
-  // example query: GET https://lmi-outlooks-esdc-edsc-apicast-production.api.canada.ca/clmix-wsx/gcapis/outlooks?noc=1111&rtp=1&rid=10
-
-  try {
-    const response = await fetch(
-      `https://lmi-outlooks-esdc-edsc-apicast-production.api.canada.ca/clmix-wsx/gcapis/outlooks?noc=${noc}&rtp=1&rid=${provinceId}&lang=en`,
-      { headers }
-    )
-    const data = await response.json()
-    return data
-  } catch (e) {
-    console.error(e)
-  }
-}
-
-/**
- * Verifies an outlook potential value form 0 to 3.
- * @param {Number} potential - The outlook potential, represented as a value from 0 to 3.
- * @returns The verbose description of the outlook potential.
- */
-function verbifyOutlookValue(potential) {
-  switch (Number(potential)) {
-    case 1:
-      return 'Limited'
-    case 2:
-      return 'Fair'
-    case 3:
-      return 'Good'
-    default:
-      return 'Undetermined'
-  }
-}
