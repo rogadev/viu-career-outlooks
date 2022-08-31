7d42b7fecf9f5ed31378e6a8711e3972f9e7a859
Roga <ryanroga@gmail.com>
2022-08-31 15:10:07 -0700

    Refactor & Enhance Error Handling
    Refactored. Mothballs removed. Now throwing SvelteKit error objects for enhanced error handling.

--- src/routes/program/[nid]/+page.server.js
+++ src/routes/program/[nid]/+page.server.js
@@ -1,53 +1,15 @@
-/** @type {string | null} */
-let programTitle = null
-/** @type {string | null} */
-let credential = null
-/** @type {Object[]} */
-let jobResults = []
+import { error } from '@sveltejs/kit'
+import viuPrograms from '$lib/server/data/viu_programs.json'
 
-/** @type {import('@sveltejs/kit').PageServerLoad<{ nid: string | number }>} */
+/** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-  const nid = params.nid
-  if (!nid) throw new Error("@migration task: Migrate this return statement (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292699)");
-  return { status: 404 }
-
-  try {
-    await fetchJobs(nid)
-  } catch (e) {
-    console.error(e)
-  }
-
-  if (!jobResults.length)
-    return {
-  jobs: [],
-  title: 'Program Not Found.',
-  credential: '',
-}
-
-  return {
-  jobs: jobResults,
-  title: programTitle,
-  credential,
-}
-}
-
-async function fetchJobs(/** @type {string | number} */ nid) {
-  try {
-    const response = await fetch(`/api/v1/jobs-program-program/${nid}`)
-    if (response.status !== 200) return { success: false }
-    console.log('asdf', await response.json())
-    const { jobs, program } = await response.json()
-
-    if (jobs.length > 0) {
-      jobResults = jobs
-    }
-
-    if (program) {
-      programTitle = program.title
-      credential = program.credential
-    }
-  } catch (e) {
-    console.error(e)
-    return { success: false }
-  }
+  let nid = params.nid
+  if (!nid) throw error(404, 'Missing nid parameter')
+  if (typeof Number.parseInt(nid) !== 'number')
+    throw error(404, 'Received bad nid parameter type')
+  const program = viuPrograms.find(
+    (program) => program.nid === Number.parseInt(nid) ?? false
+  )
+  if (!program) throw error(404, `No program found for nid: ${nid}`)
+  return { nid }
 }
--- src/routes/program/[nid]/+page.svelte
+++ src/routes/program/[nid]/+page.svelte
@@ -1,25 +1,45 @@
 <script>
-  throw new Error("@migration task: Add data prop (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292707)");
-
   import H1 from '$lib/components/viu/H1.svelte'
   import Filter from '$lib/components/Filter.svelte'
+  import P from '$lib/components/viu/P.svelte'
 
-  export let /** @type {{noc:string,title:string}[]}*/ jobs
-  export let /** @type {string}*/ title
-  export let /** @type {string} */ credential
+  export let data
+  const { nid } = data
+
+  /**
+   * @typedef Job
+   * @property {string} title
+   * @property {string} noc
+   */
+
+  /** @type {string}  */ let title
+  /** @type {string} */ let credential
+  /** @type {Job[]} */ let jobs
+
+  const fetchPrograms = async () => {
+    const response = await fetch(`/api/v1/jobs-by-program/${nid}`)
+    const data = await response.json()
+    title = data.program.title
+    credential = data.program.credential
+    jobs = data.jobs
+  }
 </script>
 
 <svelte:head>
   <title>{title} | Career Opportunties</title>
 </svelte:head>
 
-<H1>{title} {credential}</H1>
+{#await fetchPrograms()}
+  <P>loading...</P>
+{:then}
+  <H1>{title} {credential}</H1>
 
-{#if !jobs.length}
-  <p>
-    No results found for this credential, at this time. Contact the Web Team if
-    you think there's been a mistake.
-  </p>
-{:else}
-  <Filter haystack={jobs} />
-{/if}
+  {#if !jobs.length}
+    <p>
+      No results found for this credential, at this time. Contact the Web Team
+      if you think there's been a mistake.
+    </p>
+  {:else}
+    <Filter haystack={jobs} />
+  {/if}
+{/await}
