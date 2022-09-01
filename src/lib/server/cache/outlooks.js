451e3caf3636d84ba200ed81a40e4564cc351ab4
vonroga <ryanroga@gmail.com>
2022-09-01 16:59:41 -0700

    Updated outlook cache

--- src/lib/server/cache/outlooks.js
+++ src/lib/server/cache/outlooks.js
@@ -1,5 +1,5 @@
 import NodeCache from 'node-cache'
 const ttl = 60 * 60 * 24 * 30 * 2 // 2 month time to live
-const outlooksCache = new NodeCache({ stdTTL: ttl })
+const cache = new NodeCache({ stdTTL: ttl })
 
-export default outlooksCache
+export default cache
