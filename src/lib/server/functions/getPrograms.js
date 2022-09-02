219c52b87e7bc79e668b00598cca6d625212a35a
vonroga <ryanroga@gmail.com>
2022-09-01 17:00:49 -0700

    Update getPrograms.js
    Not fetching from api

--- src/lib/server/functions/getPrograms.js
+++ src/lib/server/functions/getPrograms.js
@@ -1,6 +1,7 @@
-import programs from '../data/viu_programs.json'
-
-export default function getPrograms() {
+export default async function getPrograms() {
+  const response = await fetch('/api/v1/programs')
+  const programs = await response.json()
+  // @ts-ignore
   const info = programs.map(({ nid, title, credential, program_area }) => {
     return {
       nid,
