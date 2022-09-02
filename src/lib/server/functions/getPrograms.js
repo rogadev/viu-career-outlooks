557fceba2e4a79d0215297d8d38586a32d192bdc
Roga <ryanroga@gmail.com>
2022-09-02 09:56:12 -0700

    Update getPrograms.js
    Documentation

--- src/lib/server/functions/getPrograms.js
+++ src/lib/server/functions/getPrograms.js
@@ -1,3 +1,7 @@
+/**
+ * Uses the internal API to get the programs for a given user. This internal endpoint returns the contents of the server cache and calls the VIU endpoint to update the cache.
+ * @returns {Promise<{nid:string, title:string, credential: string, program: string}[]>} - Array of programs
+ */
 export default async function getPrograms() {
   const response = await fetch('/api/v1/programs')
   const programs = await response.json()
