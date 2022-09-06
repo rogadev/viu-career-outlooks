ef9c5847c4b65f744a076a1d9b11340a3c2ed413
vonroga <ryanroga@gmail.com>
2022-09-06 15:29:19 -0700

    Fix Issue with FS
    Attempting to fix read/write issue by removing fs altogether. Now relying only on cache and fetch from viu endpoint.

--- src/routes/api/v1/programs/+server.js
+++ src/routes/api/v1/programs/+server.js
@@ -1,5 +1,3 @@
-import { writeFileSync } from 'fs'
-
 // CACHE
 import NodeCache from 'node-cache'
 const ttl = 60 * 60 * 2 // 2 hour time to live
@@ -7,14 +5,9 @@ const cache = new NodeCache({ stdTTL: ttl })
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
-  if (!cache.has('programs')) {
+  if (!cache.has('programs') || !cache) {
     const response = await fetch('https://www.viu.ca/searchable-programs.json')
     cache.set('programs', await response.json())
-    writeFileSync(
-      'src/lib/server/data/viu_programs.json',
-      JSON.stringify(cache.get('programs'), null, 2),
-      { flag: 'w' }
-    )
   }
   return new Response(JSON.stringify(cache.get('programs')))
 }
